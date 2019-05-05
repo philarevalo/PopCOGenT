25c7f0c104d99301465e9b7ed6a5feab902c64e4
Phil Arevalo <parevalo@uchicago.edu>
2019-05-05 13:40:37 -0500

    Run script

---
+++ src/PopCOGenT/PopCOGenT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+configfile=config.sh
+source ${configfile}
+source activate HGT_cluster
+source ${mugsy_env}
+
+
+if [ "${slurm_str}" = "" ]
+	then
+		python get_alignment_and_length_bias.py --genome_dir ${genome_dir} --genome_ext ${genome_ext} --alignment_dir ${alignment_dir} --mugsy_path ${mugsy_path} --mugsy_env ${mugsy_env} --base_name ${base_name} --final_output_dir ${final_output_dir} --num_threads ${num_threads}
+		python cluster.py --base_name ${base_name} --length_bias_file ${final_output_dir}/${base_name}.length_bias.txt --output_directory ./infomap_out --infomap_path ${infomap_path} ${single_cell}
+fi
