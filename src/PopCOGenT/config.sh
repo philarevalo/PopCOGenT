f17e4889cd26f7201027624bd701a835193aba11
Phil Arevalo <parevalo@uchicago.edu>
2019-05-05 13:40:49 -0500

    Cleaned up config script

--- src/PopCOGenT/config.sh
+++ src/PopCOGenT/config.sh
@@ -23,7 +23,6 @@ alignment_dir=/home/parevalo/testing/${base_name}_ssd_align/
 # Output directory for final length bias file
 final_output_dir=./
 
-
 # Are you running on a single machine? Please specify the number of threads
 num_threads=4
 
@@ -33,16 +32,3 @@ slurm_str=''
 # If using slurm, please specify the output directory for the runscripts and source scripts. Absolute paths required.
 script_dir=''
 source_path=''
-
-
-
-source activate HGT_cluster
-source ${mugsy_env}
-
-if [ "${slurm_str}" = "" ]
-	then
-		python get_alignment_and_length_bias.py --genome_dir ${genome_dir} --genome_ext ${genome_ext} --alignment_dir ${alignment_dir} --mugsy_path ${mugsy_path} --mugsy_env ${mugsy_env} --base_name ${base_name} --final_output_dir ${final_output_dir} --num_threads ${num_threads}
-		python cluster.py --base_name ${base_name} --length_bias_file ${final_output_dir}/${base_name}.length_bias.txt --output_directory ./infomap_out --infomap_path ${infomap_path} ${single_cell}
-fi
-
-
